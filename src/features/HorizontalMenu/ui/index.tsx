3642af89311bd9905ca03c738f5a5d4f6fcf6b04
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-02-03 00:40:14 +0600

    Version 1.0.1

---
+++ src/features/HorizontalMenu/ui/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+export const HorizontalMenu = () => {
+  return <aside className={styles.horizontal_menu}>index</aside>;
+};
