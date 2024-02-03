03f990cb3fc0d7583aa1a9441d34a7c498f145a1
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-02-04 00:40:11 +0600

    Version 1.0.2

---
+++ src/shared/ui/MenuButton/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+interface MenuButtonProps {}
+
+export const MenuButton = () => {
+  return <button className={styles.menu_button}>index</button>;
+};
