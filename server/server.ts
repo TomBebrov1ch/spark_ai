d5c7ca4a13365f39789c39b522b6784fcd0d8abc
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-02-07 14:43:57 +0600

    Version 1.0.8

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,22 @@
 const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.get("/", (res: any) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "SPARK_AI",
+  password: "2005",
+  port: 5432,
+});
